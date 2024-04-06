7f13c3bb16e86c1a82ccdef92397183128b64d83
PhatLeTien <tienphataki@gmail.com>
2024-04-06 17:12:53 +0700

    my-app reactjs

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Mẹ</h1>
+      <p>
+        Mẹ là tiếng hát câu thơ<br/>
+        Ru con vào những giấc mơ tuyệt vời<br/>
+        Du dương hai tiếng à ơi<br/>
+        Đong đưa con ngủ, suốt đời không quên
+      </p>
+      <p>
+        Mỗi ngày con lớn khôn lên<br/>
+        Trong vòng tay mẹ êm đềm thiết tha<br/>
+        Mẹ là tổ ấm mái nhà<br/>
+        Tình thương, cao cả, bao la, dạt dào
+      </p>
+      <p>
+        Lời mẹ dạy bảo ngọt ngào<br/>
+        Hiện tử, âu yếm, xiết bao ân tình<br/>
+        Mẹ là nắng ấm bình mình<br/>
+        Là bông hoa cúc đẹp xình sáng ngời<br/>
+      </p>
+    </div>
+  );
+}
+
+export default App;
